7b3230b3f95f2f2f7300ce625b74838f1473968c
Michelle <miss_michelle@me.com>
2024-07-10 14:45:53 -0700

    code for subway schedule api

---
+++ src/App.tsx
@@ -0,0 +1,132 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  SelectGroup,
+} from './select'
+import './styles/app.css'
+import { parseISO, format, set } from 'date-fns'
+import React, { useEffect, useState } from 'react'
+
+interface Station {
+  id: number
+  name: string
+}
+
+interface ArrivalTimes {
+  id: number
+  time: string
+}
+
+export default function App() {
+  const [stations, setStations] = useState<Station[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [northArrivalTimes, setNorthArrivalTimes] = useState<string[]>([])
+  const [southArrivalTimes, setSouthArrivalTimes] = useState<string[]>([])
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/jonthornton/MTAPI/master/data/stations.json')
+      .then((res) => res.json())
+      .then((data) => {
+        const stationsArray = Object.entries(data).map(([id, station]) => ({
+          id: Number(id),
+          name: (station as Station).name,
+        }))
+        setStations(stationsArray)
+      })
+  }, [])
+
+  const fetchArrivalTimes = ({ id }: { id: number }) => {
+    setIsLoading(true)
+    const fetchUrl = `https://cors-anywhere.herokuapp.com/https://api.wheresthefuckingtrain.com/by-id/${id}`
+    fetch(fetchUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        const arrivalsObject = data.data[0]
+        const northArrivals = arrivalsObject.N.map((arrivalTime: ArrivalTimes) => arrivalTime.time)
+        const southArrivals = arrivalsObject.S.map((arrivalTime: ArrivalTimes) => arrivalTime.time)
+        setNorthArrivalTimes(northArrivals)
+        setSouthArrivalTimes(southArrivals)
+        setIsLoading(false)
+      })
+      .catch((error) => {
+        console.error('Error fetching arrival times:', error)
+      })
+  }
+
+  const handleValueChange = (value: string) => {
+    setIsLoading(true)
+    const selectedStation = stations.find(station => station.name === value)
+    if (selectedStation) {
+      fetchArrivalTimes({ id: selectedStation.id })
+    }
+  }
+
+  return (
+    <>
+      <h1>NYC Subway Arrival Times</h1>
+      <div className='content-wrapper'>
+        <div className='stations'>
+          <h2>Choose a station:</h2>
+          <div className='stations-list'>
+            <Select onValueChange={handleValueChange}>
+              <SelectTrigger className='w-[180px]'>
+                <SelectValue placeholder='Select a station' />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  {stations.map((station) => (
+                    <SelectItem
+                      key={station.id}
+                      value={station.name}
+                    >
+                      {station.name}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+        <div className='schedule'>
+          {isLoading ? (
+            <div className='loader'>
+              <div className='loading-ellipsis'>
+                <div /><div /><div /><div />
+              </div>
+            </div>
+          ) : (
+            <>
+              <div className='arrival-times'>
+                {northArrivalTimes.length > 0 && (
+                  <>
+                    <h2>Northbound Arrival Times:</h2>
+                    {northArrivalTimes.map((time, index) => {
+                      const dateObject = parseISO(time);
+                      const formattedTime = format(dateObject, 'h:mm a');
+                      return <div key={index} className='time'>{formattedTime}</div>;
+                    })}
+                  </>
+                )}
+              </div>
+              <div className='arrival-times'>
+                {southArrivalTimes.length > 0 && (
+                  <>
+                    <h2>Southbound Arrival Times:</h2>
+                    {southArrivalTimes.map((time, index) => {
+                      const dateObject = parseISO(time);
+                      const formattedTime = format(dateObject, 'h:mm a');
+                      return <div key={index} className='time'>{formattedTime}</div>;
+                    })}
+                  </>
+                )}
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ src/select.tsx
@@ -0,0 +1,127 @@
+import {
+  CaretSortIcon,
+  CheckIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+} from '@radix-ui/react-icons'
+import * as SelectPrimitive from '@radix-ui/react-select'
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
+
+const Select = SelectPrimitive.Root
+const SelectGroup = SelectPrimitive.Group
+const SelectValue = SelectPrimitive.Value
+
+const SelectTrigger = forwardRef<
+  ElementRef<typeof SelectPrimitive.Trigger>,
+  ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
+>(({ className, children, ...props }, ref) => (
+  <SelectPrimitive.Trigger
+    ref={ref}
+    className='select-trigger'
+    {...props}
+  >
+    {children}
+  </SelectPrimitive.Trigger>
+))
+SelectTrigger.displayName = SelectPrimitive.Trigger.displayName
+
+const SelectScrollUpButton = forwardRef<
+  ElementRef<typeof SelectPrimitive.ScrollUpButton>,
+  ComponentPropsWithoutRef<typeof SelectPrimitive.ScrollUpButton>
+>(({ className, ...props }, ref) => (
+  <SelectPrimitive.ScrollUpButton
+    ref={ref}
+    className='select-scroll-up-button'
+    {...props}
+  >
+    <ChevronUpIcon />
+  </SelectPrimitive.ScrollUpButton>
+))
+SelectScrollUpButton.displayName = SelectPrimitive.ScrollUpButton.displayName
+
+const SelectScrollDownButton = forwardRef<
+  ElementRef<typeof SelectPrimitive.ScrollDownButton>,
+  ComponentPropsWithoutRef<typeof SelectPrimitive.ScrollDownButton>
+>(({ className, ...props }, ref) => (
+  <SelectPrimitive.ScrollDownButton
+    ref={ref}
+    className='select-scroll-down-button'
+    {...props}
+  >
+    <ChevronDownIcon />
+  </SelectPrimitive.ScrollDownButton>
+))
+SelectScrollDownButton.displayName =
+  SelectPrimitive.ScrollDownButton.displayName
+
+const SelectContent = forwardRef<
+  ElementRef<typeof SelectPrimitive.Content>,
+  ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
+>(({ className, children, position = 'popper', ...props }, ref) => (
+  <SelectPrimitive.Portal>
+    <SelectPrimitive.Content
+      ref={ref}
+      className='select-content'
+      position={position}
+      {...props}
+    >
+      <SelectScrollUpButton />
+      <SelectPrimitive.Viewport className='select-viewport'>
+        {children}
+      </SelectPrimitive.Viewport>
+      <SelectScrollDownButton />
+    </SelectPrimitive.Content>
+  </SelectPrimitive.Portal>
+))
+SelectContent.displayName = SelectPrimitive.Content.displayName
+
+const SelectLabel = forwardRef<
+  ElementRef<typeof SelectPrimitive.Label>,
+  ComponentPropsWithoutRef<typeof SelectPrimitive.Label>
+>(({ className, ...props }, ref) => (
+  <SelectPrimitive.Label
+    ref={ref}
+    className='select-label'
+    {...props}
+  />
+))
+SelectLabel.displayName = SelectPrimitive.Label.displayName
+
+const SelectItem = forwardRef<
+  ElementRef<typeof SelectPrimitive.Item>,
+  ComponentPropsWithoutRef<typeof SelectPrimitive.Item>
+>(({ className, children, ...props }, ref) => (
+  <SelectPrimitive.Item
+    ref={ref}
+    className='select-item'
+    {...props}
+  >
+    <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
+  </SelectPrimitive.Item>
+))
+SelectItem.displayName = SelectPrimitive.Item.displayName
+
+const SelectSeparator = forwardRef<
+  ElementRef<typeof SelectPrimitive.Separator>,
+  ComponentPropsWithoutRef<typeof SelectPrimitive.Separator>
+>(({ className, ...props }, ref) => (
+  <SelectPrimitive.Separator
+    ref={ref}
+    className='select-separator'
+    {...props}
+  />
+))
+SelectSeparator.displayName = SelectPrimitive.Separator.displayName
+
+export {
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectLabel,
+  SelectItem,
+  SelectSeparator,
+  SelectScrollUpButton,
+  SelectScrollDownButton,
+}
